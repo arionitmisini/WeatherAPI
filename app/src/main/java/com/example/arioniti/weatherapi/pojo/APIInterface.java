5ede2d18e1391eae7104ce73afaff18cdceefb3d
arionitmisini <arionitmisini@gmail.com>
2018-01-19 23:24:53 +0100

    Plain Old Java Object

---
+++ app/src/main/java/com/example/arioniti/weatherapi/pojo/APIInterface.java
@@ -0,0 +1,14 @@
+package com.example.arioniti.weatherapi.pojo;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by Arioniti on 19/01/18.
+ */
+
+public interface APIInterface {
+
+}
