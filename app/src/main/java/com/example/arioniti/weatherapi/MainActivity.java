0742a9c2cf892fbeeca75318beb87fbe32e9e950
arionitmisini <arionitmisini@gmail.com>
2018-01-19 23:27:29 +0100

    Base_url

--- app/src/main/java/com/example/arioniti/weatherapi/MainActivity.java
+++ app/src/main/java/com/example/arioniti/weatherapi/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.arioniti.weatherapi;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import retrofit2.Retrofit;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String BASE_URL = "http://api.openweathermap.org/";
+    private static Retrofit retrofit = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
