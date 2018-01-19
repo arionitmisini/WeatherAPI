2cab823ac730ba1f35c6a47dbfd8a85abae1280d
arionitmisini <arionitmisini@gmail.com>
2018-01-19 23:29:13 +0100

    Create Retrofit object

--- app/src/main/java/com/example/arioniti/weatherapi/MainActivity.java
+++ app/src/main/java/com/example/arioniti/weatherapi/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.arioniti.weatherapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import retrofit2.Retrofit;
 
+import com.example.arioniti.weatherapi.pojo.APIInterface;
+import com.example.arioniti.weatherapi.pojo.Weather;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -14,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+
+
+
 
     }
 }
