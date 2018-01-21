5a3102048f968f029ab7773053dfba1d1e0a5192
arionitmisini <arionitmisini@gmail.com>
2018-01-21 18:15:37 +0100

    SQLlite Update - Offline

--- app/src/main/java/com/example/arioniti/weatherapi/MainActivity.java
+++ app/src/main/java/com/example/arioniti/weatherapi/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.arioniti.weatherapi;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import com.example.arioniti.weatherapi.pojo.Weather;
+
+import java.util.ArrayList;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -13,13 +19,21 @@ import retrofit2.Response;
  */
 public class MainActivity extends AppCompatActivity {
     APIInterface apiInterface;
+    SQLiteDatabase articleDatabase;
 
-    TextView name,coord,visibility,clouds,base;
+    ArrayList<String> nameList = new ArrayList<>();
+    ArrayList<String>coordList = new ArrayList<>();
+    ArrayList<String>visibilityList= new ArrayList<>();
+
+    TextView name,coord,visibility;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        articleDatabase = this.openOrCreateDatabase("Weather",MODE_PRIVATE,null);
+        articleDatabase.execSQL("CREATE TABLE IF NOT EXISTS weather (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, coord VARCHAR, visibility VARCHAR)");
 
         name = (TextView)findViewById(R.id.nameInput);
         coord = (TextView)findViewById(R.id.coordInput);
@@ -33,12 +47,44 @@ public class MainActivity extends AppCompatActivity {
                 name.setText(response.body().getName());
                 visibility.setText(response.body().getCod()+"");
                 coord.setText(response.body().getVisibility()+"");
+
+                String sql = "INSERT INTO weather  (name, coord, visibility) VALUES (?, ?, ?)";
+                SQLiteStatement statement = articleDatabase.compileStatement(sql);
+                statement.bindString(1,name.getText().toString());
+                statement.bindString(2,coord.getText().toString());
+                statement.bindString(3,visibility.getText().toString());
+
+                statement.execute();
             }
             @Override
             public void onFailure(Call<Weather> call, Throwable t) {
 
             }
         });
+        updateListView();
+    }
+    public void updateListView(){
+        Cursor c = articleDatabase.rawQuery("Select * from weather",null);
+
+        int nameIndex= c.getColumnIndex("name");
+        int coordIndex = c.getColumnIndex("coord");
+        int visibilityIndex = c.getColumnIndex("visibility");
+
+        if(c.moveToFirst()){
+            nameList.clear();
+            coordList.clear();
+            visibilityList.clear();
+
+            do{
+                nameList.add(c.getString(nameIndex));
+                coordList.add(c.getString(coordIndex));
+                visibilityList.add(c.getString(visibilityIndex));
+            }while(c.moveToNext());
+        }
+
+        name.setText(nameList.get(0).toString());
+        visibility.setText(visibilityList.get(0).toString());
+        coord.setText(coordList.get(0).toString());
 
     }
 }
